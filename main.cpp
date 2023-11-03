ac9b39a99edf3b45ab53d8b36bca2d966804d873
usefhanii <usefha00@gmail.com>
2023-11-03 16:28:39 +0300

    edited main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+void main(){
+    int n,i;
+    printf("Enter the number of elements you want in array: ");
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0; i<n; i++){
+        printf("\n Enter element %d : ",i+1);
+    }
+    }
